75d73aee821a35ffcc93104535bd5054b2e0b379
Sherwyn <sherwyn.lj.goh@gmail.com>
2024-07-24 15:56:22 +0800

    Add slack foreign key and installation settings to user

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,11 @@
-from sqlmodel import SQLModel, Field
-import uuid
 from datetime import datetime
-
+from sqlmodel import SQLModel, Field, JSON
+import uuid
+from typing import  Optional
 
 class User(SQLModel, table=True):
   __tablename__ = "users"
   id: uuid.UUID = Field(default=uuid.uuid4, primary_key=True)
   created_at: datetime = Field(default=datetime.now)
+  slack_user_id: str = Field(unique=True)
+  slack_installation_settings: Optional[dict] = Field(nullable=True, sa_type=JSON, default={})
